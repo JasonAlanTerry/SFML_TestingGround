1796ddea785f5a1587e2c1abce98bad8be5e283a
DESKTOP-99S7LSF\jay <jasonalanterry@gmail.com>
2017-07-04 05:08:18 -0600

    Implement res loading

--- ResManager/ResourceHolder.cpp
+++ ResManager/ResourceHolder.cpp
@@ -1,4 +1,5 @@
 #include "ResourceHolder.h"
+#include <iostream>
 
 namespace ResCodex {
 
@@ -12,7 +13,35 @@ namespace ResCodex {
 
 	}
 
+	// TODO-- extract DRY code
 	void ResourceHolder::handleStateRequest(State::ResReq req) {
+
+		sf::Clock c;
+		float dt;
+
+		std::clog << "Loading Textures" << std::endl;
+		for (auto i : req.textures) {
+			loadTextureResource(texturePath + i);
+		}
+
+		dt = c.restart().asSeconds();
+		std::clog << "Loading Textures done | Time Elapsed: " << dt << "ms" << std::endl;
+		std::clog << "Loading Fonts" << std::endl;
+
+		for (auto i : req.fonts) {
+			loadFontResource(fontPath + i);
+		}
+
+		dt = c.restart().asSeconds();
+		std::clog << "Loading fonts done | Time Elapsed: " << dt << "ms" << std::endl;
+		std::clog << "Loading Sounds" << std::endl;
+
+		for (auto i : req.sounds) {
+			loadSoundResource(audioPath + i);
+		}
+
+		dt = c.restart().asSeconds();
+		std::clog << "Loading sounds done | Time Elapsed: " << dt << "ms" << std::endl;
 
 	}
 
--- State/SplashState.cpp
+++ State/SplashState.cpp
@@ -4,8 +4,6 @@
 
 namespace State {
 
-
-
 	SplashState::SplashState(Application& app)
 		: BaseState(app)
 	{
